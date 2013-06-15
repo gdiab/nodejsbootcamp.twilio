bea7e595f10cc3f4b758d6be47671276872d742a
gdiab <me@georgediab.com>
2013-06-15 14:22:14 -0700

    typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var twilio = require('twilio');
 var port = process.env.port || 1337;
 app.use(express.bodyParser());
 
-app.get('/'), function(req, res) {
+app.get('/', function(req, res) {
     //Validate that this request really came from Twilio...
     res.send('send an SMS message "play" to (702) 800-7236 to begin', 'Content-Type':'text/xml'}, 200);
     
