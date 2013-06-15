d7bc8e762edf900f80578473425271f7510b2880
gdiab <me@georgediab.com>
2013-06-15 14:46:10 -0700

    revert

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post('/respondToSMS', function(req, res) {
     var client = new twilio.RestClient('ACb5691b2b28019bf5f5f00647fba3e2a5', '289eadd98bc62261424c3b0e1fab21ce');
     client.sms.messages.create({
         to:'+17028584082',
-        from: fromNumber.toString(),
+        from: '17028584082',
         body: sms.toString()
     }, function(error, message) {
         
