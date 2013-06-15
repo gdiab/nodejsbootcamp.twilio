f7c7942c6eb959e54f2f37c9e0a3b2758759accb
gdiab <me@georgediab.com>
2013-06-15 11:42:47 -0700

    adding {voice:'woman'}

--- server.js
+++ server.js
@@ -8,18 +8,14 @@ app.post('/respondToVoiceCall', function(req, res) {
     if (twilio.validateExpressRequest(req, '289eadd98bc62261424c3b0e1fab21ce')) {
         var twiml = new twilio.TwimlResponse();
 
-        twiml.say('I want to win the prize. If I have to, I will punch you in the eye.');
+        twiml.say({voice:'woman'}, 'I want to win the prize. If I have to, I will punch you in the eye.');
 
         res.type('text/xml');
         res.send(twiml.toString());
     }
     else {
-        res.send('you are not twilio.  Buzz off.');
+        res.send('you are not twilio.  Buzz off. your port is: ' + port);
     }
-    //Render the TwiML document using "toString"
-    res.writeHead(200, {
-        'Content-Type':'text/xml'
-    });
     
 });
 
