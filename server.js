c410fa065c8adaaf374f23db14bf536646d1c439
gdiab <me@georgediab.com>
2013-06-15 14:07:56 -0700

    adding back lowercase

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/respondToSMS', function(req, res) {
     var sms = 'error';
     var compareString = 'i want to win the prize if I have to i will punch you in the eye';
     var twiml = new twilio.TwimlResponse();
-    if (message == compareString)
+    if (message.toLowerCase() == compareString)
     {
         sms = 'Yup! Nailed it!';
     }
