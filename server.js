fae80bb6227289164bbbb48ccf356878e16032dd
gdiab <me@georgediab.com>
2013-06-15 13:09:18 -0700

    will this work?

--- server.js
+++ server.js
@@ -30,14 +30,42 @@ app.post('/respondToSMS', function(req, res) {
     var twiml = new twilio.TwimlResponse();
     //if (body == 'I want to win the prize If I have to I will punch you in the eye'.toLowerCase())
     //{
-        twiml.say('nailed it!');
+    //    twiml.say('nailed it!');
     //}
     //else
     //{
     //    twiml.say('doh!');
     //}
-    res.type('text/xml');
-    res.send(twiml, {'Content-Type':'text/xml'}, 200);
+    //res.type('text/xml');
+    //res.send(twiml, {'Content-Type':'text/xml'}, 200);
+    var client = new twilio.RestClient('ACb5691b2b28019bf5f5f00647fba3e2a5', '289eadd98bc62261424c3b0e1fab21ce');
+    client.sms.messages.create({
+        to:'+17028584082',
+        from:'+17028007236',
+        body:'ahoy hoy! Testing Twilio and node.js'
+    }, function(error, message) {
+        
+        // The HTTP request to Twilio will run asynchronously.  This callback
+        // function will be called when a response is received from Twilio
+        
+        // The "error" variable will contain error information, if any.
+        // If the request was successful, this value will be "falsy"
+        if (!error) {
+            
+            // The second argument to the callback will contain the information
+            // sent back by Twilio for the request.  In this case, it is the
+            // information about the text messsage you just sent:
+            console.log('Success! The SID for this SMS message is:');
+            console.log(message.sid);
+     
+            console.log('Message sent on:');
+            console.log(message.dateCreated);
+        }
+        else {
+            console.log('Oops! There was an error.');
+        }
+    });
+
     
 });
 
